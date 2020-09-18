35680cd510fabacd1f28f64d33c078a842c63556
Edina Delic <edinadelic83@gmail.com>
2020-09-18 15:51:19 +0200

    Adding Dependency Container

---
+++ MyMicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using MyMicroRabbit.Domain.Core.Bus;
+using MyMicroRabbit.Infrastructure.Bus;
+
+namespace MyMicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterService(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
